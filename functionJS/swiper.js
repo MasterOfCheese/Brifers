64fe466f9d4d45da0b5a7d8e8632ed9a65f444f6
Nam Phuong <pnam24823@gmail.com>
2024-07-26 08:18:41 +0700

    carousel

---
+++ functionJS/swiper.js
@@ -0,0 +1,9 @@
+  const swiper = new Swiper('.swiper', {
+    pagination: false,
+    slidesPerView: 3,
+    spaceBetween: 30,
+  })
+
+  document.querySelector('.swiper-scrollbar').style.display = 'none'
+  document.querySelector('.swiper-button-prev').style.display = 'none'
+  document.querySelector('.swiper-button-next').style.display = 'none'
